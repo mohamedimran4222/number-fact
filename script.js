b16a0f35106eff2fe775df77e8f9466a72616825
Mohamed Imran <68016860+mohamedimran4222@users.noreply.github.com>
2021-08-29 11:09:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let fact=document.querySelector('#fact');
+let factText=document.querySelector('#fact-text');
+let numberInput=document.querySelector('#numberInput');
+
+numberInput.addEventListener('input',getFactAjax);
+
+function getFactAjax(){
+    let number=numberInput.value;
+    
+    let xhr=new XMLHttpRequest();
+    xhr.open('GET','http://numbersapi.com/'+number);
+    xhr.onload=function(){
+        if(this.status==200&&number!=''){
+            fact.style.display='block';
+            factText.innerText=this.responseText;
+        }
+    }
+    xhr.send();
+}
